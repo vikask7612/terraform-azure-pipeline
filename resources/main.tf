5f3fdc86d074ed5a34a1485245137e0b18665882
Piyush Kumar <piyush.kumar@davita.com>
2021-06-30 01:31:07 +0530

    second commit

--- resources/main.tf
+++ resources/main.tf
@@ -11,7 +11,21 @@ terraform {
     }
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resourcegroup" {
   name     = var.resource_group_name
   location = var.location
+}
+
+resource "azurerm_virtual_network" "virtualnet" {
+  name                = var.virtualnet
+  location            = var.location
+  address_space       = var.address
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet
+  virtual_network_name = azurerm_virtual_network.virtualnet.name
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  address_prefixes     = var.addressprefix
 }
--- resources/variables.tf
+++ resources/variables.tf
@@ -6,4 +6,27 @@ variable "resource_group_name" {
 variable "location" {
   type        = string
   description = "Default Resources location"
+  default     = "eastus"
+}
+
+variable "address" {
+  type        = list(string)
+  description = "Default Address"
+  default     = ["10.0.0.0/16"]
+}
+
+variable "virtualnet" {
+  type        = string
+  description = "Virtual network name"
+}
+
+variable "subnet" {
+  type        = string
+  description = "Subnet name"
+}
+
+variable "addressprefix" {
+  type        = list(string)
+  description = "Default Address"
+  default     = ["10.0.1.0/24"]
 }
